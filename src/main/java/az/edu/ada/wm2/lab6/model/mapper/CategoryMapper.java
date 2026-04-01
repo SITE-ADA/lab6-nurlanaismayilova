2867da672dd7045929cfabbf2c824e32e34fbbe9
nurlanaismayilova <150562064+nurlanaismayilova@users.noreply.github.com>
2026-04-02 01:54:10 +0400

    Add Mappers

---
+++ src/main/java/az/edu/ada/wm2/lab6/model/mapper/CategoryMapper.java
@@ -0,0 +1,25 @@
+package az.edu.ada.wm2.lab6.model.mapper;
+
+import az.edu.ada.wm2.lab6.model.Category;
+import az.edu.ada.wm2.lab6.model.dto.CategoryRequestDto;
+import az.edu.ada.wm2.lab6.model.dto.CategoryResponseDto;
+
+import java.util.UUID;
+
+public class CategoryMapper {
+
+    public static Category toEntity(CategoryRequestDto dto){
+        Category category = new Category();
+        category.setId(UUID.randomUUID());
+        category.setName(dto.getName());
+        return category;
+    }
+
+    public static CategoryResponseDto toResponseDto(Category category)
+    {
+        CategoryResponseDto dto = new CategoryResponseDto();
+        dto.setId(category.getId());
+        dto.setName(category.getName());
+        return dto;
+    }
+}
