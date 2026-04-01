1a3fa07c42addd18e261c849ed73b3c6080c0884
nurlanaismayilova <150562064+nurlanaismayilova@users.noreply.github.com>
2026-04-02 01:10:41 +0400

    Add CategoryEntity and CategoryRepository

--- src/main/java/az/edu/ada/wm2/lab6/model/Category.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/Category.java
@@ -2,13 +2,29 @@ package az.edu.ada.wm2.lab6.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 public class Category {
     @Id
     private UUID id;
     private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+
+    public Category(String name) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+    }
 }
--- src/main/java/az/edu/ada/wm2/lab6/model/Product.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/Product.java
@@ -1,13 +1,14 @@
 package az.edu.ada.wm2.lab6.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @NoArgsConstructor
@@ -20,6 +21,13 @@ public class Product {
     private String productName;
     private BigDecimal price;
     private LocalDate expirationDate;
+    @ManyToMany
+    @JoinTable(
+            name = "product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name="category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
 
     public Product(String productName, BigDecimal price, LocalDate expirationDate) {
         this.id = UUID.randomUUID();
@@ -27,4 +35,6 @@ public class Product {
         this.price = price;
         this.expirationDate = expirationDate;
     }
+
+
 }
